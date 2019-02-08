b77eddd425b5f75ca2ea53e0d1ebde82af529dd8
chankruze <chankruze@gmail.com>
2019-02-08 22:40:08 +0530

    Initial Commit

---
+++ app/src/main/java/in/geekofia/viewpager/Fragments/SundayFragment.java
@@ -0,0 +1,20 @@
+package in.geekofia.viewpager.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.geekofia.viewpager.R;
+
+/**
+ * Fragment that displays "Sunday".
+ */
+public class SundayFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sunday, container, false);
+    }
+}
